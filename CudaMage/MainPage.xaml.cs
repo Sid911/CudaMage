4fcef37fafd13f01b34039414cc29282d04a8599
Siddharth Sinha <siddharthsinha9752@gmail.com>
2021-03-22 18:32:12 +0530

    inital commit

---
+++ CudaMage/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CudaMage
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void StartDll_Click(object sender, RoutedEventArgs e)
+        {
+            GridView grid = new GridView();
+            int[] res = CudaAdd.getNumbers();
+            for (int i = 0; i < 16; i++)
+            {
+                grid.Items.Add(res[i]);
+            }
+            ResultGrid.Children.Add(grid);
+        }
+    }
+}
+
+class CudaAdd
+{
+    [DllImport("CudaTest.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "GETRANDOMARRAYSUM")]
+    unsafe public static extern void GETRANDOMARRAYSUM(
+        [MarshalAs(UnmanagedType.LPArray)] int[] c,
+        int N
+        );
+
+    public static int[] getNumbers()
+    {
+        int[] c = new int[16];
+        int[]a;
+        GETRANDOMARRAYSUM(c,16);
+        return c;
+    }
+}
---
+++ CudaTest/kernel.cu
@@ -0,0 +1,76 @@
+﻿
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+#include <cassert>
+#include <iostream>
+
+using std::cout;
+
+// CUDA kernel for vector addition
+__global__ void vectorAdd(int* a, int* b, int* c, int N) {
+	// Calculate global thread thread ID
+	int tid = (blockDim.x * blockIdx.x) + threadIdx.x;
+
+	// Boundary check
+	if (tid < N) {
+		c[tid] = a[tid] + b[tid];
+	}
+}
+
+extern "C" __declspec(dllexport) void GETRANDOMARRAYSUM(int c[], int N ) {
+
+	int *a, *b, *d;
+	size_t bytes = N * sizeof(int);
+
+	// Allocation memory for these pointers
+	cudaMallocManaged(&a, bytes);
+	cudaMallocManaged(&b, bytes);
+	cudaMallocManaged(&d, bytes);
+
+	// Get the device ID for prefetching calls
+	int id = cudaGetDevice(&id);
+
+	// Set some hints about the data and do some prefetching
+	cudaMemPrefetchAsync(d, bytes, id);
+
+	for (int i = 0; i < N; i++) {
+		a[i] = i+1;
+		b[i] = i+1;
+	}
+
+	// Pre-fetch 'a' and 'b' arrays to the specified device (GPU)
+	cudaMemAdvise(a, bytes, cudaMemAdviseSetReadMostly, id);
+	cudaMemAdvise(b, bytes, cudaMemAdviseSetReadMostly, id);
+	cudaMemPrefetchAsync(a, bytes, id);
+	cudaMemPrefetchAsync(b, bytes, id);
+
+	// Threads per CTA (1024 threads per CTA)
+	int BLOCK_SIZE = 1 << 10;
+
+	// CTAs per Grid
+	int GRID_SIZE = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+	// Call CUDA kernel
+	vectorAdd << <GRID_SIZE, BLOCK_SIZE >> > (a, b, d, N);
+
+	cudaDeviceSynchronize();
+
+	// Prefetch to the host (CPU)
+	cudaMemPrefetchAsync(a, bytes, cudaCpuDeviceId);
+	cudaMemPrefetchAsync(b, bytes, cudaCpuDeviceId);
+	cudaMemPrefetchAsync(d, bytes, cudaCpuDeviceId);
+
+	for (int i = 0; i < N; i++)
+	{
+		c[i] = a[i] + b[i];
+	}
+
+
+	// Free unified memory (same as memory allocated with cudaMalloc)
+	cudaFree(a);
+	cudaFree(b);
+	cudaFree(d);
+
+}
